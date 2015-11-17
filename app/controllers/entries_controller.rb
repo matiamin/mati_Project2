4fbcf76afe6443aab87df9212768025fba05caf7
Matiullah Amin <matiullah243@gmail.com>
2015-11-17 14:07:02 -0500

    user model with signup/login/logout functionatlities

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,10 @@
 class EntriesController < ApplicationController
 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
 def index
+  # authenticate_user!
   @entries = Entry.all
 end
 
