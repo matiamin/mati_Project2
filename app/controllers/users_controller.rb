a9c14c99dbdfb3319eab10efc0735286cf5b0646
Matiullah Amin <matiullah243@gmail.com>
2015-11-18 23:25:40 -0500

    profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
 
-  end
+def show
+  @user = User.find(params[:id])
+  @entries = @user.entries
+
+end
+
 end
