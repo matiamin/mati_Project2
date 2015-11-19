e8e857fa750f20d2351a342ad34c7ac281b9b718
Matiullah Amin <matiullah243@gmail.com>
2015-11-19 14:02:05 -0500

    contact us form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get 'page/policy'
 
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
+
+
   resources :users, only: [:show]
 
   devise_for :users, :path => 'accounts'
